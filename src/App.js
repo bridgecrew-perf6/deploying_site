7e004708e6ddaa25a8e7154bf15620364c330b6a
Furrukh Sohail <furrukh.khan333@gmail.com>
2022-05-23 12:02:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import JsonData from "./MOCK_DATA.json";
+import ReactPaginate from "react-paginate";
+import  AOS  from "aos";
+import 'aos/dist/aos.css'
+
+function App() {
+  useEffect(()=>{
+    AOS.init({duration:2500});
+  },[])
+  const [users] = useState(JsonData.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <div data-aos="fade-left" className="user">
+          <h3>{user.firstName}</h3>
+          <h3>{user.lastName}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <div className="App">
+      <h1 data-aos="fade-right">React-Pagination</h1>
+      {displayUsers}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+    </div>
+  );
+}
+
+export default App;
